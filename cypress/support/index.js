bcfbb430a878a60ecf08479242291e7da3e8d47e
sahil28051 <sahil@testrig.co.in>
2022-05-13 19:03:24 +0530

    buildspec

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -22,7 +22,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-import '@percy/cypress'
+
 
 Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from failing the test
